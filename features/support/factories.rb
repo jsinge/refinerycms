6863d0d361ac3980f9b26e72663efa9ff1e5db01
Azul <azul@riseup.net>
2010-08-18 12:23:14 +0200

    weitere features und erste step definitionen

---
+++ features/support/factories.rb
@@ -0,0 +1,26 @@
+require 'factory_girl'
+
+Factory.define :user do |u|
+  u.sequence(:login) { |n| "person#{n}" }
+  u.sequence(:email) { |n| "person#{n}@cucumber.com" }
+  u.password  "greenandjuicy"
+  u.password_confirmation "greenandjuicy"
+end
+
+Factory.define :refinery_user, :parent => :user do |u|
+  u.roles { [ Role[:refinery] ] }
+  u.after_create do |user|
+    Refinery::Plugins.registered.each_with_index do |plugin, index|
+      user.plugins.create(:name => plugin.name, :position => index)
+    end
+  end
+end
+
+Factory.define :vendor, :parent => :user do |u|
+  u.roles { [ Role[:vendor] ] }
+  u.after_create do |user|
+    Refinery::Plugins.registered.each_with_index do |plugin, index|
+      user.plugins.create(:name => plugin.name, :position => index)
+    end
+  end
+end
