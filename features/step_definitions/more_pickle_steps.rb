080c6a9e353d7bb02fa162411aec9b38b7f6cfb6
Azul <azul@riseup.net>
2010-08-18 12:48:40 +0200

    setting some attributes and associations in steps

---
+++ features/step_definitions/more_pickle_steps.rb
@@ -0,0 +1,17 @@
+# additional steps to what pickle offers. Please add your steps here as
+#   pickle_steps.rb
+# might be overwritten in a pickle upgrade
+
+# set an attribute to another model
+
+Given(/^#{capture_model}(?:'s)? (\w+) is #{capture_model}$/) do |name, association, ref|
+  model!(name).send(association) = model!(ref)
+end
+
+Given(/^#{capture_model}(?:'s)? (\w+) is "([^"])"$/) do |name, attribute, value|
+  model!(name).send(attribute) = value
+end
+
+Given(/^#{capture_model}(?:'s)? (\w+) include #{capture_model}$/) do |name, association, ref|
+  model!(name).send(association) << model!(ref)
+end
