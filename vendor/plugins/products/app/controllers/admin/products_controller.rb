bc57e76284b281a840a4828fa3d333f9cae25fbe
jsinge <jonsinge@freenet.de>
2010-08-23 13:06:47 +0200

    adopt to crud revert. conditions don't require quotes anymore

--- vendor/plugins/products/app/controllers/admin/products_controller.rb
+++ vendor/plugins/products/app/controllers/admin/products_controller.rb
@@ -2,7 +2,7 @@ class Admin::ProductsController < Admin::BaseController
 
   crudify :product, :title_attribute => :title,
     :include => [:shop],
-    :conditions => '"shops.user_id = #{current_user.id}"',
+    :conditions => 'shops.user_id = #{current_user.id}',
     :order => 'shops.position,products.position ASC'
 
 end
--- vendor/plugins/shops/app/controllers/admin/shops_controller.rb
+++ vendor/plugins/shops/app/controllers/admin/shops_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ShopsController < Admin::BaseController
 
   crudify :shop, :title_attribute => :title,
-    :conditions => '{ :user_id => current_user }'
+    :conditions => 'user_id = #{current_user.id}'
 
 end
