4794225be906e2fff30d6ff237e25f8608b5fd47
Azul <azul@riseup.net>
2010-08-24 12:16:58 +0200

    adding pickle's paths so we can do "on the product's page"

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -49,6 +49,21 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+
+    # the following are examples using path_to_pickle
+
+    when /^#{capture_model}(?:'s)? page$/                           # eg. the forum's page
+      path_to_pickle $1
+
+    when /^#{capture_model}(?:'s)? #{capture_model}(?:'s)? page$/   # eg. the forum's post's page
+      path_to_pickle $1, $2
+
+    when /^#{capture_model}(?:'s)? #{capture_model}'s (.+?) page$/  # eg. the forum's post's comments page
+      path_to_pickle $1, $2, :extra => $3                           #  or the forum's post's edit page
+
+    when /^#{capture_model}(?:'s)? (.+?) page$/                     # eg. the forum's posts page
+      path_to_pickle $1, :extra => $2                               #  or the forum's edit page
+
     else
       # Loads in path_to functions from the engines that Refinery is using.
       # They should look like this:
