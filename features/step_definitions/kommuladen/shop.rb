9ebad9cdf6e0a34e147e2d1f4216f0aa03d59df1
Azul <azul@riseup.net>
2010-08-24 11:34:56 +0200

    make the shop and product creation more beautiful

---
+++ features/step_definitions/kommuladen/shop.rb
@@ -0,0 +1,10 @@
+# Create a shop with a user
+Given(/^"([^"]*)" runs a shop "([^"]*)" /) do |owner, shop|
+  Given "a user: \"#{login}\" exists with login: \"#{login}\", name: \"#{owner}\""
+  Given "a shop: \"#{shop.underscore}\" exists with title: \"#{shop}\", user: that user"
+end
+
+Given(/^#{capture_model} sells "([^"]*)" for (\d*)/) do |shop, product, price|
+  Given "a product: \"#{product.underscore}\" exists with title: \"#{product}\", price: \"#{price}\", shop: #{shop}"
+end
+
