19b7d85a4ddf233ce2990b783107ae2c4ef3622a
jsinge <jonsinge@freenet.de>
2010-08-25 16:18:49 +0200

    routes: shop has_many products and categories

--- vendor/plugins/shops/config/routes.rb
+++ vendor/plugins/shops/config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :shops
+
+  map.resources :shops, :has_many => [:products, :categories]
 
   map.namespace(:admin, :path_prefix => 'refinery') do |admin|
     admin.resources :shops, :collection => {:update_positions => :post}
