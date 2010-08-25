c09399828985fd3b1ba13743349e83601477125c
jsinge <jonsinge@freenet.de>
2010-08-25 18:55:49 +0200

    fixed conditions bug for categories, analog to pages

--- vendor/plugins/categories/app/controllers/admin/categories_controller.rb
+++ vendor/plugins/categories/app/controllers/admin/categories_controller.rb
@@ -1,7 +1,6 @@
 class Admin::CategoriesController < Admin::BaseController
 
   crudify :category, :title_attribute => :title,
-          :conditions => "'categories'.parent_id IS NULL",
           :order => "position ASC",
           :paging => false
           
