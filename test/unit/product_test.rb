11fecb28bfcd272ed3f1c4d7884c07d505a8314a
Azul <azul@riseup.net>
2010-08-25 10:38:43 +0200

    test the product scopes used for listings

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -24,4 +24,30 @@ class ProductTest < ActiveSupport::TestCase
     end
   end
 
+  def test_listing_scopes
+    first = Factory.create :product
+    shop = first.shop
+    second = Factory.create :product, :shop => shop
+    third = Factory.create :product, :shop => shop
+    other = Factory.create :product
+
+    assert_equal [first, second, third], shop.products
+    assert_equal [first, second, third], Product.limit(3)
+    assert_not_equal [first, second, third], Product.randomize.limit(3)
+    assert_equal 3, shop.products.randomize.count
+    assert_equal 4, Product.randomize.count
+    assert_equal 3, Product.without(first).count
+    assert_equal 2, Product.limit(2).all.count
+    assert_equal 3, Product.limit(3).without(first).all.count
+  end
+
+  def test_similarity_scopes
+    peach = Factory.create :product, :tag_list => "fruit, orange, tasty"
+    orange = Factory.create :product, :tag_list => "fruit, orange, tasty"
+    apple = Factory.create :product, :tag_list => "fruit, green, tasty"
+    spaghetti = Factory.create :product, :tag_list => "food, noodles, tasty"
+    bike = Factory.create :product, :tag_list => "object, traffic, fun"
+    assert_equal [orange, apple, spaghetti], Product.similar_to(peach).without(peach)
+    assert_equal [3, 3, 2, 1], Product.similar_to(peach).map(&:similarity)
+  end
 end
