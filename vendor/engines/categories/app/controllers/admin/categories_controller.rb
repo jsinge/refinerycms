00479ef6424dcb4bde28111404e8b0d85d53a156
jsinge <jonsinge@freenet.de>
2010-09-22 18:05:24 +0200

    backend users can only access their own categories

--- vendor/engines/categories/app/controllers/admin/categories_controller.rb
+++ vendor/engines/categories/app/controllers/admin/categories_controller.rb
@@ -3,5 +3,6 @@ class Admin::CategoriesController < Admin::BaseController
   crudify :category, :title_attribute => :title,
           :order => "position ASC",
           :paging => false
+  limit_to_shop :category       
           
 end
